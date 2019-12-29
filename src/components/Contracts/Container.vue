11169db97a6025d31a6e71572fe0ea1af270e811
Daniel Agg <daniel.agg@outlook.com>
2019-12-29 16:56:32 +0100

    Scrollbar

--- src/components/Contracts/Container.vue
+++ src/components/Contracts/Container.vue
@@ -2,21 +2,36 @@
   <div class="wrapper">
     <TopBar />
     <div class="container">
-      <ContractList class="contract-list" />
+      <ContractList v-if="contracts.length" :contracts="contracts" class="contract-list" />
       <ContractDetail class="contract-detail" />
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import TopBar from "./TopBar";
 import ContractList from "./ContractList";
 import ContractDetail from "./ContractDetail";
+
 export default {
   components: {
     TopBar,
     ContractList,
     ContractDetail
+  },
+  data() {
+    return {
+      contracts: [],
+      selectedContract: null
+    };
+  },
+  async created() {
+    const result = await axios.get(
+      "/api/sapdata/Get?query=ContractHeadSet?$filter=((CompanyCode eq 'Z200')) and ((ValidityStart eq '20181229'))&$top=10&$inlinecount=allpages"
+    );
+    this.contracts = result.data.d.results.slice(0, 10);
+    this.selectedContract = this.contracts[0];
   }
 };
 </script>
@@ -32,10 +47,10 @@ export default {
   margin: 0 auto;
 }
 .contract-list {
-  width: 25%;
+  width: 30%;
 }
 .contract-detail {
-  width: 75%;
-  margin: 3rem 0;
+  width: 70%;
+  margin: 3rem 0 3rem 1rem;
 }
 </style>
--- src/components/Contracts/ContractList.vue
+++ src/components/Contracts/ContractList.vue
@@ -1,21 +1,75 @@
 <template>
-  <h1>List</h1>
+  <ul>
+    <li v-for="(item, index) in contracts" :key="index">
+      <div>
+        <h1>{{ item.AgreementNo }}</h1>
+        <p class="supplier">
+          Supplied by
+          <span class="supplier-name">{{ item.CompanyCodeDesc }}</span>
+        </p>
+      </div>
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
-  name: "ContractList",
-  data() {
-    return {
-      contracts: [
-        { id: 1234567, supplier: "1324" },
-        { id: 1234565, supplier: "1324" },
-        { id: 1234563, supplier: "1324" },
-        { id: 1234562, supplier: "1324" }
-      ]
-    };
+  props: {
+    contracts: {
+      type: Array,
+      required: true
+    }
   }
 };
 </script>
 
-<style></style>
+<style scoped>
+ul {
+  list-style-type: none;
+  margin-top: 4.5rem;
+  padding: 0 0 0 1.4em;
+  overflow: scroll;
+  overflow-x: hidden;
+  max-height: 70vh;
+}
+
+li {
+  margin-top: 2em;
+  cursor: pointer;
+  opacity: 0.5;
+}
+
+li:hover {
+  opacity: 1;
+}
+
+li:active {
+  opacity: 1;
+}
+
+li:first-child {
+  margin-top: 0;
+}
+
+h1 {
+  font-size: 1.3rem;
+}
+
+::-webkit-scrollbar {
+  width: 6px;
+}
+
+::-webkit-scrollbar-track {
+  background: #e4e4e4;
+  border-radius: 10px;
+}
+
+::-webkit-scrollbar-thumb {
+  background: var(--lighter-gray);
+  border-radius: 10px;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: var(--gray);
+}
+</style>
